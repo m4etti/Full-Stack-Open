b892beafa8ff6d228b93b411f4197d6f5e402802
m4etti <matti.vout@gmail.com>
2023-10-20 12:35:40 +0300

    Osa 2 tehtävät 2.16-2.17 puhelinluettelo

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -3,27 +3,42 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Numbers from './components/Numbers'
 import personService from './services/persons'
+import Notification from './components/Notification'
 
 
 const App = () => {
+  // State hooks for managing application data
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-  const [filter, setfilter] = useState('')
+  const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState({ text: "", type: "" })
 
-  //console.log(personService)
+  // State hook for managing the need to refresh data from the server
+  const [refreshNeeded, setRefreshNeeded] = useState(true)
 
+  // Effect hook for initializing persons data or refreshing it when needed
   useEffect(() => {
-    console.log('initialize persons')
-    personService
-      .getAll()
-      .then(initialPersons => {
-        setPersons(initialPersons)
-      })
-  }, [])
+    if (refreshNeeded) {
+      console.log('initialize persons')
+      personService
+        .getAll()
+        .then(initialPersons => {
+          setPersons(initialPersons)
+          setRefreshNeeded(false) // Data has been refreshed
+        })
+    }
+  }, [refreshNeeded])
 
-  console.log('render', persons.length, 'persons')
+  // Function for displaying notifications with a timeout
+  const setNotification = (content) => {
+    setMessage(content)
+    setTimeout(() => {
+      setMessage({ text: "", type: "" })
+    }, 5000)
+  }
 
+  // Function to handle adding a new person to the phonebook
   const addPerson = (event) => {
     event.preventDefault()
 
@@ -34,23 +49,28 @@ const App = () => {
       }
 
       const existingPerson = persons.find(person => person.name === newName)
-      //console.log(existingPerson)
 
+      // Check if person already exists and confirm update
       if (existingPerson && window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
         console.log("person already in phonebook")
         personService.update(existingPerson.id, personObject)
           .then(initialPersons => {
+            // Update the persons state with the updated person
             setPersons(persons.map(person => person.id !== existingPerson.id ? person : initialPersons))
             console.log(`Person  ${existingPerson.name} was edited`)
           })
           .catch(error => {
-            console.error(`Error Editting person: ${error}`)
+            console.error(`Error editting person: ${error}`)
+            setNotification({ text: `Error editting person: ${newName}`, type: "error" })
+            setRefreshNeeded(true) // Trigger data refresh after an error
           })
 
       }
       else {
+        // Create a new person
         personService.create(personObject).then(initialPersons => {
-          setPersons(persons.concat(initialPersons))
+          setPersons(persons.concat(initialPersons)) // Add the new person to the persons state
+          setNotification({ text: `${personObject.name} added`, type: "success" })
           setNewName("")
           setNewNumber("")
           console.log(`person ${JSON.stringify(personObject)} added`)
@@ -58,41 +78,44 @@ const App = () => {
       }
     }
     else {
-      alert(`Enter name and number`)
+      setNotification({ text: `Enter name and number`, type: "error" })
     }
   }
 
-  const handelNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handelNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
-
-  const handelFilterChange = (event) => {
-    setfilter(event.target.value)
-  }
-
-  const personsToShow = persons.filter(person =>
-    person.name.toLowerCase().includes(filter.toLowerCase()))
-
+  // Function to handle removing a person from the phonebook
   const removePerson = (id, name) => (() => {
     console.log("trying to remove person")
     if (window.confirm(`Delete ${name}?`)) {
       personService
         .remove(id)
         .then(() => {
-          setPersons(persons.filter(person => person.id !== id));
+          setPersons(persons.filter(person => person.id !== id)); // Remove the person from the persons state
+          setNotification({ text: `Person ${name} removed`, type: "success" })
           console.log(`Person ${name} removed from the server`);
         })
         .catch(error => {
           console.error(`Error removing person: ${error}`);
+          setRefreshNeeded(true) // Trigger data refresh after an error
         })
     }
   }
   )
 
+  // ... (handlers for input changes and filtering)
+  const handelNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handelNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handelFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase()))
+
+
+  // JSX rendering of the application components
   return (
     <div>
       <h2>Phonebook</h2>
@@ -105,6 +128,7 @@ const App = () => {
         onNumberChange={handelNumberChange}
         onSubmit={addPerson}
       />
+      <Notification message={message} />
       <h3>Numbers</h3>
       <Numbers persons={personsToShow} removePerson={removePerson} />
     </div>
