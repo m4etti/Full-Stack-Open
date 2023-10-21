efcc1f32267509a2296b6d90baea8f337988fd32
m4etti <matti.vout@gmail.com>
2023-10-21 13:30:55 +0300

    apiKey removed

--- Osa 2/maiden tiedot/src/services/weather.js
+++ Osa 2/maiden tiedot/src/services/weather.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const ApiKey = import.meta.env.VITE_WEATHER_API_KEY
-//const ApiKey = `a439de23f180692e4fa7106a8b3a3322`
 
 const getWeather = ([lat, lon]) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${ApiKey}&units=metric`
