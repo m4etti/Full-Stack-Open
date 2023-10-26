c5286df6db909243f53e588d3be2ea443fd094b3
m4etti <matti.vout@gmail.com>
2023-10-26 22:05:47 +0300

    3.14: puhelinluettelo ja tietokanta, step2

--- Osa 3/puhelinluettelo/Backend/index.js
+++ Osa 3/puhelinluettelo/Backend/index.js
@@ -6,31 +6,6 @@ const Person = require('./models/person')
 
 const app = express()
 
-// let persons = [
-//     {
-//         id: 1,
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: 2,
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: 3,
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: 4,
-//         name: "Mary Poppendick",
-//         number: "39-23-6423122"
-//     }
-// ]
-
-
-
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
@@ -45,7 +20,6 @@ const requestLogger = (request, response, next) => {
         console.log('Status Code:', response.statusCode);
         console.log('---');
     });
-
     next()
 }
 
@@ -114,19 +88,19 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const personExists = persons.find(person => person.name === body.name);
-    if (personExists) {
-        return response.status(400).json({ error: 'name must be unique' });
-    }
+    // const personExists = persons.find(person => person.name === body.name);
+    // if (personExists) {
+    //     return response.status(400).json({ error: 'name must be unique' });
+    // }
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 app.use(unknownEndpoint)
