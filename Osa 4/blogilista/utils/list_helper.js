025c286efe8732085d8cc6f66651434feabfee65
m4etti <matti.vout@gmail.com>
2023-10-31 14:01:32 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- Osa 4/blogilista/utils/list_helper.js
+++ Osa 4/blogilista/utils/list_helper.js
@@ -1,7 +1,16 @@
-const dummy = (blogs) => {
+const dummy = () => {
     return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+}
+
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+
+    return blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
+}
