3806ecbc37be1ae91808fdbaeb42f8bb42b94791
m4etti <matti.vout@gmail.com>
2023-11-01 14:59:04 +0200

    4.11*: blogilistan testit, step4

--- Osa 4/blogilista/tests/blog_api.test.js
+++ Osa 4/blogilista/tests/blog_api.test.js
@@ -24,24 +24,37 @@ test('GET /api/blogs', async () => {
     expect(response.body[0].id).toBeDefined()
 })
 
-test('POST /api/blogs', async () => {
-    const newBlog = {
-        title: 'Test blog',
-        author: 'test test',
-        url: 'http://test.com',
-        likes: 2,
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('POST /api/blogs', () => {
+    test('new blog is added', async () => {
+        const newBlog = {
+            title: 'Test blog',
+            author: 'test test',
+            url: 'http://test.com',
+            likes: 2,
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    const blogs = blogsAtEnd.map(blog => blog.title)
-    expect(blogs).toContain('Test blog')
+        const blogs = blogsAtEnd.map(blog => blog.title)
+        expect(blogs).toContain('Test blog')
+    })
+
+    test('if likes not given 0 likes', async () => {
+        const newBlog = {
+            title: 'Test blog',
+            author: 'test test',
+            url: 'http://test.com',
+        }
+
+        const response = await api.post('/api/blogs').send(newBlog)
+        expect(response.body.likes).toEqual(0)
+    })
 })
 
 
