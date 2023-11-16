28866075a7fe9b5b6f42c10a3176fa259e9c0908
m4etti <matti.vout@gmail.com>
2023-11-17 00:13:40 +0200

    6.8: anekdootit, step6 fix

--- Osa 6/anecdotes-redux/src/components/AnecdoteList.jsx
+++ Osa 6/anecdotes-redux/src/components/AnecdoteList.jsx
@@ -1,27 +1,43 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnectode } from '../reducers/anecdoteReducer'
+import PropTypes from 'prop-types'
 
-const AnecdoteList = () => {
-    let anecdotes = useSelector(state => state)
+const Anecdote = ({ anecdote }) => {
     const dispatch = useDispatch()
-    anecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
     const vote = (id) => {
         console.log('vote', id)
         dispatch(voteAnectode(id))
     }
     return (
+        <div >
+            <div>
+                {anecdote.content}
+            </div>
+            <div>
+                has {anecdote.votes}
+                <button onClick={() => vote(anecdote.id)}>vote</button>
+            </div>
+        </div>
+    )
+}
+
+Anecdote.propTypes = {
+    anecdote: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        votes: PropTypes.number.isRequired,
+    }).isRequired,
+}
+
+const AnecdoteList = () => {
+    let anecdotes = useSelector(state => state)
+    anecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
+
+    return (
         <div>
             {anecdotes.map(anecdote =>
-                <div key={anecdote.id}>
-                    <div>
-                        {anecdote.content}
-                    </div>
-                    <div>
-                        has {anecdote.votes}
-                        <button onClick={() => vote(anecdote.id)}>vote</button>
-                    </div>
-                </div>
+                <Anecdote key={anecdote.id} anecdote={anecdote} />
             )}
         </div>
     )
